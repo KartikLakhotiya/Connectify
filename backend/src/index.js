354a708d3f9429b9f1155eaeeeba3ad8494a5332
Kartik  Lakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2025-01-06 17:14:36 +0530

    user authentication completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.routes.js"
 import connectDB from "./lib/MongoDB.js";
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 
