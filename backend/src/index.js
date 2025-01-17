2f7de5f44777cacf675ad3a1cd88b8842ae69d4c
Kartik  Lakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2025-01-17 20:02:51 +0530

    changes in codebase for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,8 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -22,6 +24,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.get('/', (req, res) => {
     res.send("Connectify server is ready.");
